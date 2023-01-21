baf351982e108a0c39155465c9ce2c75c7bba1d3
goodiekoo <goodiekoo@gmail.com>
2023-01-21 14:22:55 -0700

    revised BS4 code (it tested on NAVER)

--- main.py
+++ main.py
@@ -1,33 +1,21 @@
-from urllib.parse import quote_plus
+import bs4
 from bs4 import BeautifulSoup
-from selenium import webdriver
+import requests
+import sys
+import webbrowser
 
-baseUrl = 'https://www.google.com/search?q='
-plusUrl = input('검색할 CEO 이름 (반드시 영문):') #코드 테스트용, 최종버전에서는 csv 파일을 자동으로 읽어서 처리할 예정
-url = baseUrl + quote_plus(plusUrl)
-#url = baseUrl + quote_plus(plusUrl)
+base_url = "https://search.naver.com/search.naver?where=view&sm=tab_jum&query="
+keyword = input('검색할 CEO 이름 (반드시 영문):')
+#base_url = 'https://www.linkedin.com/search/results/people/?keywords='
+#base_url = 'https://www.google.com/search?q='+''.join(sys.argv[1:]))  #구글은 별도 검색 url 따와야함
+#코드 테스트용, 최종버전에서는 csv 파일을 자동으로 읽어서 처리할 예정
 
-driver = webdriver.Chrome()
-driver.get(url)
+search_url = base_url + keyword
 
-html = driver.page_source
-soup = BeautifulSoup(html,"html.parser") #parser로 지정해줘야함
+r = requests.get(search_url)
 
-#selenium 그대로 사용 코드인데 결과가 안나옴
-r = soup.select('.r')
-for i in r:
-    print(i.select_one('.ellip').text)
-    print(i.select_one('.iUh30.bc').text)
-    print(i.a.attrs['href'])
-    print()
+soup = bs4.BeautifulSoup(r.text,"html.parser")
 
-#사용 안함
-#r = soup.select('GLI8Bc UK95Uc')
-
-#for i in r:
-#    print(i.select_one('.ellip').text)
-#    print(i.select_one('.LC20lb MBeuO DKV0Md').text)
-#    print(i.a.attrs['href'])
-#    print()
-
-driver.close()
+items = soup.select(".api_txt_lines.total_tit")
+for e, item in enumerate(items, 1):
+    print(f"{e} : {item.text}")
